0f74aac8e85fa2489b2869d9bdc200600f21debd
pradeep-nsz <pradeep@nsolutionz.com>
2015-07-20 14:52:20 +0530

    first commit

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,25 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.brodysoft.sqlitePlugin/www/SQLitePlugin.js",
+        "id": "com.brodysoft.sqlitePlugin.SQLitePlugin",
+        "clobbers": [
+            "SQLitePlugin"
+        ]
+    },
+    {
+        "file": "plugins/com.phonegap.plugins.facebookconnect/facebookConnectPlugin.js",
+        "id": "com.phonegap.plugins.facebookconnect.FacebookConnectPlugin",
+        "clobbers": [
+            "facebookConnectPlugin"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.brodysoft.sqlitePlugin": "1.0.6",
+    "com.phonegap.plugins.facebookconnect": "0.11.0"
+}
+// BOTTOM OF METADATA
+});
